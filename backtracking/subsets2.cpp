b22a0e2d523f6c94c1b38741e2cb6124018bcdd9
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-09 21:29:01 +0530

    Update subsets2.cpp

--- backtracking/subsets2.cpp
+++ backtracking/subsets2.cpp
@@ -1,31 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-void bt( vector<vector<int>>&  res, vector<int>& curr,vector<int>& nums, int index){
-                        res.push_back(curr);
-                        for(int i=index;i<nums.size();i++){
-                            if(i> index && nums[i]==nums[i-1])continue;
-                            curr.push_back(nums[i]);
-                            bt(res, curr, nums,i+1  );
-                            curr.pop_back();
-                        }
-}
-
-vector<vector<int>> subsets2(vector<int> &nums){
-    vector<vector<int>> res;
-    vector<int> curr;
-    sort(nums.begin() , nums.end());
-    bt(res , curr , nums, 0 );
-    return res;
-}
-
-int main(){
-     vector<int> nums={1, 2, 1};
-     vector<vector<int>> finalx = subsets2(nums);
-     for(int i=0;i<finalx.size();i++){
-       for(int x: finalx[i]){
-         cout<<x<<" ";
-       }
-       cout<<endl;
-     }
-}
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+            sort(nums.begin(), nums.end());
+            vector<vector<int> >res;
+            vector<int> current;
+            bt(res, current, 0, nums);
+            return res;
+    }
+    void bt(vector<vector<int>>& res, vector<int>& current, int index, vector<int>& nums) {
+        res.push_back(current);
+        for(int i=index;i<nums.size();i++){
+                if(i> index && nums[i]==nums[i-1]){
+                    continue;
+                }
+                current.push_back( nums[i] );
+                bt(res, current, i+1, nums);
+                current.pop_back();
+        }
+    }
+};
