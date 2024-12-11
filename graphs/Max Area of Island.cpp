5528df6267f9845de729272aaa0a415bd32f5413
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-11 20:32:44 +0530

    Create Max Area of Island.cpp

---
+++ graphs/Max Area of Island.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    void dfs(int &count, int i , int j, vector<vector<int>> &grid){
+        int m=grid.size();
+          int n=grid[0].size();
+
+                if(i< 0 || j<0 || i> m-1 || j> n-1 || grid[i][j]==0){
+                    return ;
+                }
+                grid[i][j]=0;
+                count++;
+                dfs(count, i+1, j, grid);
+                  dfs(count, i, j+1, grid);
+                    dfs(count, i, j-1, grid);
+                      dfs(count, i-1, j, grid);
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+          int m=grid.size();
+          int n=grid[0].size();
+          int maxx=0;
+          for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                  if(grid[i][j]!=0){
+                        int count=0;
+                        dfs(count, i, j, grid);
+                        maxx=max(maxx, count);
+                  }
+            }
+          }
+          return maxx;
+    }
+};
