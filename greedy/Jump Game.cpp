23820274b5f58276c484813900d6f5da92b5693c
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-14 19:47:48 +0530

    Create Jump Game.cpp

---
+++ greedy/Jump Game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       
+        // 2 0 1 1
+
+         int n=nums.size();
+         int index=n-1;
+         for(int i=n-1;i>=0;i--){
+                if(nums[i] + i  >=  index){
+                       index=i;
+                }
+         }
+         if(index==0)return true;
+         else return false; 
+    }   
+};
