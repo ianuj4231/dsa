1c8134cec0d04fa6783d5e64a73f366a96bf830b
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-16 19:25:12 +0530

    Update Binary Tree Level Order Traversal.cpp

--- tree/Binary Tree Level Order Traversal.cpp
+++ tree/Binary Tree Level Order Traversal.cpp
@@ -1,48 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct TreeNode{
-      int val;
-      TreeNode* left;
-      TreeNode* right;
-      TreeNode(int x){
-        val=x;
-        left=nullptr;
-        right=nullptr;
-      }
-};
-
-void lvl(TreeNode* root){
-    vector<vector<int>> all;
-    queue<TreeNode* > q;
-    q.push(root);
-    while(! q.empty()){
-        int size=q.size();
-        vector<int> res;
-        while(size--){
-          TreeNode* curr=  q.front();
-          q.pop();
-          if(curr->left){
-             q.push(curr->left);
-          }
-          if(curr->right){
-               q.push(curr->right);
-          }
-          res.push_back(curr->val);
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+         vector<vector<int>>  finalx;
+         queue<TreeNode* > q;
+         if(!root){
+            return {};
+         }
+        q.push(root);
+        while(! q.empty()){
+            vector<int> currx;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode* curr = q.front();
+                q.pop();
+                if(curr->left){
+                        q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
+                currx.push_back(curr->val);
+                }
+            
+            finalx.push_back(currx);
         }
-        all.push_back(res);
-    }
-    for(int i=0;i< all.size();i++){
-      for(int j: all[i]){
-          cout<< j<<" ";
-      }
-      cout<<endl;
-    }
-}
+            return finalx;
 
-// time is o-N 
-int main(){
-  TreeNode* root= new TreeNode(1);
-  root->left=new TreeNode(0);
-  root->right=new TreeNode(3);
-  lvl(root);
-}
+    }
+    
+};
