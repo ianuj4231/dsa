60edf1d177b42c9e13ad678774eb813e84d5a213
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-16 19:02:49 +0530

    Update Validate Binary Search Tree.cpp

--- tree/Validate Binary Search Tree.cpp
+++ tree/Validate Binary Search Tree.cpp
@@ -1,29 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct TreeNode{
-      int val;
-      TreeNode* left;
-      TreeNode* right;
-      TreeNode(int x){
-        val=x;
-        left=nullptr;
-        right=nullptr;
-      }
-};
-
-bool val(TreeNode* root, int minn, int maxx){
-        if(!root)return true;
-        if(root->val  <=  minn || root->val >= maxx  ){
-          return false;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool  fn(  TreeNode* root, long long minn, long long maxx  )  
+{
+        if(!root){
+            return true;
         }
-        return val(root->left,  minn, root->val) &&  val(root->right,   root->val, maxx ); 
-     
-}
+        if( root->val <= minn  || root->val >= maxx ){
+            return false;
+        }
+        return fn(root->left, minn, root->val) && fn(root->right, root->val, maxx);
 
-int main(){
-  TreeNode* root= new TreeNode(1);
-  root->left=new TreeNode(4);
-  root->right=new TreeNode(3);
-  cout<< val(root, INT_MIN, INT_MAX );   // prints 1 for true and 0 for false
-  return 0;
 }
+    bool isValidBST(TreeNode* root) {
+            return fn(  root, LONG_MIN, LONG_MAX  );    
+    }
+};
