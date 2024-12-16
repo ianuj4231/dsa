252b53227515ee7ceff91099b6e1ea13660f10a8
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-16 12:39:05 +0530

    Create n-queens.cpp

---
+++ backtracking/n-queens.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+         vector<vector<string>> res;
+         vector<string> board(n, string(n,'.'));
+         set<int> cols, diag1, diag2;
+         bt(res, board, 0, n, cols, diag1, diag2);
+         return res;
+    }
+    void bt(vector<vector<string>>& res, vector<string>& board, int row, int n,
+                   set<int>& cols, set<int>& diag1, set<int>& diag2)
+    {
+           
+           if(row==n){
+            res.push_back(board);
+            return;
+           }
+           
+           for(int col=0;col<n;col++){
+            if(cols.count(col)||diag1.count(row-col)||diag2.count(row+col)){
+                 continue;
+            }
+           
+            board[row][col]='Q';
+            cols.insert(col);
+            diag1.insert(row - col);
+            diag2.insert(row + col);        
+            
+            bt(res, board, row + 1, n, cols, diag1, diag2);
+
+            board[row][col] = '.';
+            cols.erase(col);
+            diag1.erase(row - col);
+            diag2.erase(row + col);
+        }
+    }
+};
