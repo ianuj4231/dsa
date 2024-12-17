0ebcbbece5f8c4b19b49cbc31d418949ee3e8152
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-17 12:06:44 +0530

    Update Binary Tree Right Side View.cpp

--- tree/Binary Tree Right Side View.cpp
+++ tree/Binary Tree Right Side View.cpp
@@ -1,39 +1,35 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct TreeNode{
-      int val;
-      TreeNode* left;
-      TreeNode* right;
-      TreeNode(int x){
-        val=x;
-        left=nullptr;
-        right=nullptr;
-      }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*  > q;
+        q.push
+        (root);
+        vector<int> res;
+        while(! q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+            TreeNode* curr=q.front();
+            q.pop();
+            if(curr && curr->left){
+                q.push(curr->left);
+            } 
+            if(curr && curr->right){
+                q.push(curr->right);
+            }
+            if(curr && i==size-1) res.push_back(curr->val);
+            }
+        }
+        return  res;
+    }
 };
-
-void right(TreeNode* root)
-{
-  queue<TreeNode* > q;
-  q.push(root);
-  vector<int> res;
-  while(! q.empty() ){
-       int sz=q.size();
-       for(int i=0;i<sz;i++){
-         TreeNode* curr= q.front();
-      q.pop();
-      if(i==sz-1)res.push_back(curr->val);
-      if(curr->left)q.push(curr->left);
-      if(curr->right)q.push(curr->right);  
-       }
-       
-  }
-  for(int x: res)cout<<x<<" "; 
-}
-
-int main(){
-    TreeNode* root1 = new TreeNode(1);
-    root1->left = new TreeNode(0);
-    root1->right = new TreeNode(4);
-    right(root1);
-    cout<<endl;
-}
