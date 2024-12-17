b13318eaf50c5d86f1ed4820ff427d9ef03aeb57
i anuj <134675919+ianuj4231@users.noreply.github.com>
2024-12-17 16:00:39 +0530

    Update Min Cost Climbing Stairs.cpp

--- dp/Min Cost Climbing Stairs.cpp
+++ dp/Min Cost Climbing Stairs.cpp
@@ -11,6 +11,19 @@ arr[0] = 0, min cost to reach  1st step
  arr[1]= 0, 
 arr[2]= min(arr[0] + cost[0]
 
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int> dp (n+1);
+        dp[0]=0,dp[1]=0;
+        for(int i=2;i<=n;i++){
+            dp[i]=min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+            cout<<"x"<<" " << dp[i]<<endl;
+       }
+        return  dp[n ];
+    }
+};
 
 */
 class Solution {
